41e23f144f889dd3492a939850b0e130db4768b9
Aja Romporas <aya.romporas@gmail.com>
2017-10-15 21:34:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ukus Pokus | Brzi recepti od onoga što imate u kući</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	Ćiribu ćiriba! <br>
+	Neka se ukusno i zdravo jelo brzo nađe na mom stolu!<br><br>
+
+	Abrakadabra! <br>
+	Neka to jelo bude napravljeno od sastojaka koje imam u kući!<br>
+	Ukus pokus! <br>
+	I klopa je gotova!<br>
+
+
+</body>
+</html>
