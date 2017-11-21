45269e141bdeb2c81357c2f0a203ae66a116dfbf
Milanche11 <mirkovicmilan0211@gmail.com>
2017-11-21 20:35:41 +0100

    Milan
    Brisanje a taga iz  home/index.php

--- admin/views/home/index.php
+++ admin/views/home/index.php
@@ -2,6 +2,4 @@
 	<h1>Welcome To ShareBoard</h1>
 	<p class="lead">Test rada git-a</p>
 	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Start</a>
-	
-	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Start</a>
 </div>
