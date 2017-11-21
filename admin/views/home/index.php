226c297d43f7bf97afbf419322d0101614443e9a
Milanche11 <mirkovicmilan0211@gmail.com>
2017-11-21 20:17:23 +0100

    test

--- admin/views/home/index.php
+++ admin/views/home/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
 	<h1>Welcome To ShareBoard</h1>
-	<p class="lead">Find something cool? Share it with our community. Look at other shares as well</p>
+	<p class="lead">Test rada git-a</p>
 	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Share Now</a>
 </div>
