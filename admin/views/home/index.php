20bf6f3a54248d3112045fa20c538e533f1aae54
Milanche11 <mirkovicmilan0211@gmail.com>
2017-11-21 21:00:09 +0100

    Milan
    Git radi ponovo

--- admin/views/home/index.php
+++ admin/views/home/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
 	<h1>Welcome To ShareBoard</h1>
-	<p class="lead">Git radi</p>
+	<p class="lead">Git radi ponovo</p>
 	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Start</a>
 </div>
