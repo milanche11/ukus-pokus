8b6f943a94bb55b12fc874ed4cef87cf0d3ea950
Milanche11 <mirkovicmilan0211@gmail.com>
2017-11-21 20:19:20 +0100

    Naslov
    Opis sta se uradilo

--- admin/views/home/index.php
+++ admin/views/home/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
 	<h1>Welcome To ShareBoard</h1>
 	<p class="lead">Test rada git-a</p>
-	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Share Now</a>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Start</a>
 </div>
