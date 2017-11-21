ff27bdd42f31350cb650cd78e154a4a6f7b39437
Milanche11 <mirkovicmilan0211@gmail.com>
2017-11-21 20:27:06 +0100

    Milan
    Dodavanje a taga na index stranu

--- admin/views/home/index.php
+++ admin/views/home/index.php
@@ -2,4 +2,6 @@
 	<h1>Welcome To ShareBoard</h1>
 	<p class="lead">Test rada git-a</p>
 	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Start</a>
+	
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Start</a>
 </div>
