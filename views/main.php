bbc062943cd705651fecb473ced1519bb6783368
Aja Romporas <aya.romporas@gmail.com>
2017-11-27 11:38:07 +0100

    Aja

--- views/main.php
+++ views/main.php
@@ -5,7 +5,7 @@
 	<title>Ukus pokus | Brzi recepti od onoga što imate u kući</title>
 
   <!-- styling -->
-  
+  <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.css" rel="stylesheet"/>
 	<link rel="stylesheet" type="text/css" href=" <?php echo ROOT_URL; ?>assets/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href=" <?php echo ROOT_URL; ?>assets/css/style.css">
   <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo ROOT_URL; ?>assets/images/favicon/android-icon-192x192.png">
@@ -13,21 +13,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Awesomeplete JS - autocomplete javascipt -->
+  <!-- Autocomplete search -->
 
-  <link rel="stylesheet" href=" <?php echo ROOT_URL; ?>assets/css/awesomplete.css" />        
+  <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.css" rel="stylesheet"/>        
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-  <!-- Awesomeplete JS -->
-  <script src=" <?php echo ROOT_URL; ?>assets/js/awesomplete.js" async></script>   
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">  
 </head>
 
 <body>
 
   <div class="container-fluid">
-
 
     <nav class="navbar">
 
@@ -52,7 +48,6 @@
           </li>
         </ul>
       </div> 
-
     </nav>
 
     <br><br>
@@ -93,6 +88,8 @@
       </ul>
       </nav>
     </footer>
+    <!-- End footer -->
+
   </div>
 
   <!-- Optional JavaScript -->
@@ -101,6 +98,8 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+
 
 </body>
 </html>
