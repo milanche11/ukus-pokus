d3d747206dd55b0f05f5e4a0e49af6cc59ed100c
Aja Romporas <aya.romporas@gmail.com>
2017-11-29 22:20:47 +0100

    Aja

--- models/homemodel.php
+++ models/homemodel.php
@@ -27,8 +27,6 @@ class HomeModel extends Model{
   			 	
 			}
 
-			
-
 		} 
 	}
 
--- views/home/index.php
+++ views/home/index.php
@@ -1,26 +1,20 @@
 
 
-
-
+<!-- Gornji deo -->
 <div class="text-center">
 	<h1>Dobrodošli na Ukus pokus!</h1>
 
-	<p class="lead">Ovde možete pronaći proverene brze recepte od namirnica koje imate u kući</p>
-
-	<br><br>
+	<p class="lead">Ovde možete pronaći proverene brze recepte od namirnica koje imate u kući</p><br><br>
 
 	<div class="container-fluid">
-     <div class="col-lg-10 offset-lg-1">
- 
-
-    <!-- Form search -->
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.js"></script>
-
-    <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
-      
-        <label>Pretraga po namirnicama</label>
+        <div class="col-lg-10 offset-lg-1">
+     
+          <!-- Form search -->
+          <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+          <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.js"></script>
+          <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
+            
+            <label>Pretraga po namirnicama</label>
 
             <select class="form-control form-control-lg custom-select" multiple style="width: 80%" placeholder="U kući imam..." aria-label="Search for..." name="pretraga[]">
 
@@ -33,81 +27,76 @@
                 $item['ingredient_name'] . '</option>';
 
               } 
-
               ?>
-
             </select>
-
-            <button class="btn btn-success" type="submit" name="submit">
-             Traži!
-            </button>      
-    </form>
+            <button class="btn btn-success" type="submit" name="submit">Traži!</button>      
+          </form>
 
 <?php
 
-$sastojci = new HomeModel;
-
-$ingrRows = $sastojci->ingredients();
-
-//print_r($ingrRows);
-
-$query = "";
-
-if (isset($ingrRows)) {
-
-  foreach ($ingrRows as $row) {
-
-    $query .= "recipe_ingrs_id like '%" . "," .$row. "," . "%' AND ";
-  }
-
-  $query = rtrim($query, "AND ");
-
-  $recRows = $sastojci->recipes($query);
-
-    foreach ($recRows as $item) {
-      echo $item['recipe_title'] . "<br>";
-  }
-
-
-//echo $query;
-
-}
-
-
-
+  $sastojci = new HomeModel;
+  $ingrRows = $sastojci->ingredients();
+  //print_r($ingrRows);
 ?>    
 
+        </div>
+
+        <div class="container-fluid">
+          <small>Unesite prva dva slova namirnice, a zatim je izaberite iz padajućeg menija.</small>
+        </div><br><br><br>
+    <!-- End form -->  
   </div>
 
-
-<div class="container-fluid">
-  <small>Unesite prva dva slova namirnice, a zatim je izaberite iz padajućeg menija.
-  </small>
-</div>
-
-
-     
-<br><br><br>
-
-    <!-- End form -->  
-
-    </div>
-
  </div>
+
+<!-- Gornji deo kraj -->
+
+<div class="container-fluid text-center">
+
+<?php  
+   
+  //Ispis recepata po osnovu upita
+
+  $query = "";
+
+  if (isset($ingrRows)) {
+
+    foreach ($ingrRows as $row) {
+
+      $query .= "recipe_ingrs_id like '%" . "," .$row. "," . "%' AND ";
+    }
+
+    $query = rtrim($query, "AND ");
+
+    $recRows = $sastojci->recipes($query);
+
+    $numberRecipes = count($recRows);
+
+    echo "<h4 class='text-center'>Ukupno recepata koji sadrže tražene namirnice : " . "<span style='color: #28a745 !important; font-size: 2rem;'>" . $numberRecipes ."</span></h4><br>";
+
+    
+      foreach ($recRows as $item) {
+        echo '<p>';
+        echo $item['recipe_title'];
+        echo "</p>";
+    }
+
+
+  }
+?>
+
+<br><br><br>
+</div>
 
 <br><br><br>
 
 <div class="text-center">
 
 <h4>Najpopularniji recepti</h4>
-<p>
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
 <h4>Najnoviji recepti</h4>
-<p>
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-</p>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
 </div>
 
@@ -125,13 +114,6 @@ It is a long established fact that a reader will be distracted by the readable c
   evt.params.originalEvent.stopPropagation();
   });
 </script>
-
-
-
-
-
-
-
 
 
 
