eeb84d5dfe4054676e12db516474d5c9b20123f9
Milanche11 <mirkovicmilan0211@gmail.com>
2017-12-02 23:03:39 +0100

    Milan

--- admin/assets/js/functions.js
+++ admin/assets/js/functions.js
@@ -28,16 +28,6 @@
 	}
 		// Milan
 
-		function cloneFunction(){
-		/*    var para = document.getElementById("sastojak");
-		    var cln = para.cloneNode(true);
-		    document.getElementById("sastojakall").appendChild(cln);
-		*/
-			
-			alert("NESTO");
-		
-		
-		}
 
 
 		
