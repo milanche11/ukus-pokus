a3fdd3642168af23711890334a73d253867d88b6
petarcvetic <petar,cvetic@gmail.com>
2017-12-02 23:08:15 +0100

    petar

--- admin/assets/js/functions.js
+++ admin/assets/js/functions.js
@@ -28,17 +28,7 @@
 	}
 		// Milan
 
-		function cloneFunction(){
-		/*    var para = document.getElementById("sastojak");
-		    var cln = para.cloneNode(true);
-		    document.getElementById("sastojakall").appendChild(cln);
-		*/
-			
-			alert("NESTO");
 		
-		
-		}
-
 
 		
 		
