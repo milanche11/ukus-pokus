2a68782fcb45158c5da64cbdc02afd58142e7b52
petarcvetic <petar.cvetic@gmail.com>
2017-12-04 09:49:17 +0100

    petar
    Izmena dugmica "Delete" / "Activate" na strani Recepti

--- admin/views/recipes/index.php
+++ admin/views/recipes/index.php
@@ -16,16 +16,27 @@
 ?>
 	
 	<tbody>
-	    <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-	    <tr>
+		<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+			<tr>
 			 
-	      <td><?php  echo $i.'.'; $i++ ?></td>
-	      <td><a href="<?PHP ROOT_URL ?>recipes/view/<?php echo $item['recipe_id']; ?>"><?php echo $item['recipe_title']; ?></a></td>
-	      <td><?php echo date_format($date, 'Y-m-d'); ?></td>
-	      <td></td>
-	      <td>
-			<button type="delete" class="btn btn-danger btn-sm" name="delete" value="<?php echo $item['recipe_id']; ?>">Delete</button>  
-	      	<button type="button" class="btn btn-success btn-sm" name="edit">Edit</button></td>
+				<td><?php  echo $i.'.'; $i++ ?></td>
+				<td><a href="<?PHP ROOT_URL ?>recipes/view/<?php echo $item['recipe_id']; ?>"><?php echo $item['recipe_title']; ?></a></td>
+				<td><?php echo date_format($date, 'Y-m-d'); ?></td>
+				<td></td>
+				<td>
+			
+					<?php //PETAR
+					if($item['status'] == 0){	
+						echo '<button type="button" onclick="edit('."'activate','recipes','recipe_id','status','1',".$id.')" class="btn btn-warning btn-sm">Activate</button>';
+					}else{
+						echo "<button type='button' onclick='edit(".'"delete","recipes","recipe_id","status","0",'.$id.")' class='btn btn-danger btn-sm'>Delete</button>";
+					}?>   
+						<button type="button" onclick="edit(alert('PROBA'))"  class="btn btn-success btn-sm">Edit</button>
+		  
+				<!--
+					<button type="delete" class="btn btn-danger btn-sm" name="delete" value="<?php echo $item['recipe_id']; ?>">Delete</button>  
+					<button type="button" class="btn btn-success btn-sm" name="edit">Edit</button></td>
+				-->
 	    </tr>
 	</form>
 	<?php endforeach; ?>
