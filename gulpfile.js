b2bfaca80ed83b36de2d7ae0d5fbe08f38bfaab3
Aja Romporas <aya.romporas@gmail.com>
2017-12-13 20:31:49 +0100

    Aja gulp

--- gulpfile.js
+++ gulpfile.js
@@ -36,21 +36,31 @@
             baseDir: 'dist'
         },
 
-        // The port
-        port: 1234
     });
 
     // Watch for file changes
-    gulp.watch('ukus-pokus/*.html', ['html']);
+    gulp.watch('c://wamp64/www/ukus-pokus/*.html', ['watch-html']);
  });
 
  // Copies HTML from ukus-pokus to dist
 
  gulp.task('html',function(){
      return gulp
-     .src('ukus-pokus/*.php')
+     .src('c://wamp64/www/ukus-pokus/*.html')
      .pipe(gulp.dest('dist'));
  });
+
+
+/**
+ * Watch tasks
+ */
+
+gulp.task('watch-html',['html'], function (done) {
+    browser.reload();
+    done();
+});
+
+
 
 
    /**
